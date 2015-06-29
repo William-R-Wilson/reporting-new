9d040c144ae64cc732755b88b1b8cd410fa1be17
Will <williamrwilson@gmail.com>
2015-06-29 21:57:56 +0000

    mobile friendlier view

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -29,10 +29,14 @@ class ReportsController < ApplicationController
   
   def show
     @user = User.find_by(id: params[:user_id])
-    @start_date = Date.civil(params[:start_date][:year].to_i, params[:start_date][:month].to_i, params[:start_date][:day].to_i)  
-    @end_date = Date.civil(params[:end_date][:year].to_i, params[:end_date][:month].to_i, params[:end_date][:day].to_i)
+    @start_date = Date.civil(params[:start_date][:year].to_i, 
+                  params[:start_date][:month].to_i, params[:start_date][:day].to_i)  
+    @end_date = Date.civil(params[:end_date][:year].to_i, 
+                params[:end_date][:month].to_i, params[:end_date][:day].to_i)
     @user_id = params[:user_id]
-    @transactions = Transaction.where('user_id = ? AND date BETWEEN ? AND ?', @user_id, @start_date, @end_date).order(:date)
+    @transactions = Transaction.where(  'user_id = ? AND date BETWEEN ? AND ?', 
+                                        @user_id, @start_date, @end_date)
+    @transactions_for_display = @transactions.paginate(:page => params[:page]).order(:date)                                    
     @transactions_grouped_by_account = @transactions.group(:account)
     #group by program
     @transactions_grouped_by_program = @transactions_grouped_by_account.group(:program)

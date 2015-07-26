d25cedfc9c3f70c8821941e7dd74eecffb0e4050
Will <williamrwilson@gmail.com>
2015-07-26 13:40:26 -0400

    add edit and delete to accounts

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -18,7 +18,30 @@ class AccountsController < ApplicationController
     end
   end
 
+  def edit
+    @account = Account.find(params[:id])
+  end
+
+  def update
+    @account = Account.find(params[:id])
+    respond_to do |format|
+      if @account.update(account_params)
+        format.html { redirect_to accounts_url, notice: 'Account was successfully edited.' }
+        format.json { redirect_to accounts_url, status: :ok, location: @accounts }
+      else
+        format.html { render :edit }
+        format.json { render json: @account.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def destroy
+    @account = Account.find(params[:id])
+    @account.destroy
+    respond_to do |format|
+        format.html { redirect_to accounts_url, notice: "Account was successfully deleted." }
+        format.json { head :no_content }
+    end
   end
 
   def index
@@ -28,6 +51,6 @@ class AccountsController < ApplicationController
   private 
   
     def account_params
-      params.require(:account).permit(:name)
+      params.require(:account).permit(:name, :id)
     end
 end

bd9659f0d69264498821f33b761b6f4b1f045b43
Will <williamrwilson@gmail.com>
2016-11-04 21:08:55 -0400

    all transaction export to csv working

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -7,7 +7,29 @@ class Transaction < ActiveRecord::Base
   belongs_to :account
   belongs_to :program
 
-  self.per_page = 20  
+  self.per_page = 20
+
+  def self.to_csv
+    attributes = %w{employee date vendor who what where why amount account program}
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+      all.each do |transaction|
+        csv << [transaction.user.first_name,
+                transaction.date,
+                transaction.vendor,
+                transaction.who,
+                transaction.what,
+                transaction.where,
+                transaction.why,
+                transaction.amount,
+                transaction.account.name,
+                transaction.program.name]
+      end
+    end
+
+  end
+
 
 
 
